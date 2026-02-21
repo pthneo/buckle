3c9dacc28b6e89223c6e45fba98cafed642f42f0
pthneo <ben@schenk.com>
2026-02-22 09:25:01 +1000

    feat: init docs site

--- tests/core.test.ts
+++ tests/core.test.ts
@@ -1,7 +1,7 @@
 import { describe, expect, it } from "bun:test";
 
 describe("main", () => {
-	it("Test framework is working.", () => {
-		expect(true).toBe(true);
-	});
+  it("Test framework is working.", () => {
+    expect(true).toBe(true);
+  });
 });
